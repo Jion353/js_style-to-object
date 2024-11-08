ebe5e2f37c37b2a81e35822807fc74a92978bd13
Bovt Dmytro <mr.bovt2004@gmail.com>
2024-11-08 17:59:13 +0200

    done task

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,24 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const cssStyle = {};
+  const arrStr = sourceString.split(';');
+
+  for (const pieceArr of arrStr) {
+    // @ts-ignore
+    // eslint-disable-next-line no-console
+    console.log(pieceArr);
+
+    if (pieceArr) {
+      const [styleName, value] = pieceArr.trim().split(':');
+
+      if (styleName && value) {
+        cssStyle[styleName.trim()] = value.trim();
+      }
+    }
+  }
+
+  return cssStyle;
 }
 
 module.exports = convertToObject;
